460a41b203caca2832b410d0e7c08f1c7cf406f6
Benjamin Kalman <kalman@chromium.org>
2015-03-11 16:04:37 -0700

    Comment Protector: Don't show onbeforeunload alert if a form was submitted.

--- comment-protector/script.js
+++ comment-protector/script.js
@@ -6,11 +6,30 @@ function main() {
       window.location.pathname.indexOf('/pull/') == -1) {
     return;
   }
+
+  // Track whether a form is being submitted.
+  var isSubmitting = false;
+  window.addEventListener('click', function(e) {
+    var target = e.target;
+    if (target instanceof HTMLInputElement &&
+        target.type == 'submit') {
+      isSubmitting = true;
+      // No longer submitting after... 3 seconds? Maybe there is a real event
+      // to listen to here, but onbeforeunload doesn't give us a reason for the
+      // unload.
+      setTimeout(function() {
+        isSubmitting = false;
+      }, 3000);
+    }
+  });
+
   window.onbeforeunload = function() {
     var textareas = window.document.body.getElementsByTagName('textarea');
     for (var i = 0; i < textareas.length; i++) {
       var textarea = textareas[i];
-      if (textarea.offsetParent && textarea.value.length > 0)
+      if (!isSubmitting &&            // not right after the form was submitted
+          textarea.offsetParent &&    // only if the textarea is visible
+          textarea.value.length > 0)  // only if the textarea has a value
         return 'It looks like you have a comment in progress.';
     }
   };
