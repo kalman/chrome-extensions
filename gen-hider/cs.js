521227219ebd9797d34db6378c60a3eaa9497843
Benjamin Kalman <btkalman@gmail.com>
2016-04-13 15:30:00 -0700

    gen-hider: hide generate js files

--- gen-hider/cs.js
+++ gen-hider/cs.js
@@ -1,11 +1,19 @@
+function contains(str, snip) {
+  return str.indexOf(snip) !== -1;
+}
+
 function endsWith(str, suff) {
   return str.slice(str.length - suff.length, str.length) === suff;
 }
 
-[].forEach.call(document.querySelectorAll(".file.js-details-container"), function(file) {
+[].forEach.call(document.querySelectorAll('.file.js-details-container'), function(file) {
   var header = file.querySelector('.file-header');
   var path = header.dataset.path;
-  if (endsWith(path, '.noms.go') || (endsWith(path, '.go') && path.indexOf('sha1_') !== -1)) {
+  if (endsWith(path, '.noms.go') ||
+      endsWith(path, '.noms.js') ||
+      (endsWith(path, '.go') && contains(path, 'sha1_')) ||
+      (endsWith(path, '.js') && contains(path, 'sha1_'))) {
+    header.style.borderBottom = 'none';
     for (var sib = header.nextElementSibling; sib; sib = sib.nextElementSibling) {
       sib.style.display = 'none';
     }
